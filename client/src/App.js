7c4c3a212b03a578c1bd76214c94c53dfeb2a0c8
Vikash Gupta <vikas.gupta@ext.zestmoney.in>
2022-04-18 12:03:10 +0530

    FE - components update

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,37 @@
 import "./App.css";
-import { Container, AppBar, Typography } from "@mui/material";
+import {
+  Container,
+  AppBar,
+  Typography,
+  Grow,
+  Grid,
+  GridList,
+} from "@mui/material";
+import memories from "./images/memories.png";
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
 
 function App() {
   return (
-    <Container >
+    <Container maxWidth="lg">
       <AppBar position="static" color="inherit">
-        <Typography variant="h2" align="center">Memories</Typography>
+        <Typography variant="h2" align="center">
+          Memories
+        </Typography>
+        <img src={memories} alt="memories" height="60" />
       </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justifyContent="space-between" alignItems="stretch">
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
     </Container>
   );
 }
