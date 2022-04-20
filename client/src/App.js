79797f1afb0188a0aee82213821740f978de2db8
Vikash Gupta <vikas.gupta@ext.zestmoney.in>
2022-04-20 07:53:17 +0530

    FE - update

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,19 @@
 import "./App.css";
-import {
-  Container,
-  AppBar,
-  Typography,
-  Grow,
-  Grid,
-  GridList,
-} from "@mui/material";
+import { Container, Typography, Grow, Grid, GridList } from "@mui/material";
 import memories from "./images/memories.png";
-import Posts from './components/Posts/Posts';
-import Form from './components/Form/Form';
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
+import { StyledAppBar } from './styles';
 
 function App() {
   return (
     <Container maxWidth="lg">
-      <AppBar position="static" color="inherit">
+      <StyledAppBar position="static" color="inherit">
         <Typography variant="h2" align="center">
           Memories
         </Typography>
         <img src={memories} alt="memories" height="60" />
-      </AppBar>
+      </StyledAppBar>
       <Grow in>
         <Container>
           <Grid container justifyContent="space-between" alignItems="stretch">
---
+++ client/src/styles.js
@@ -0,0 +1,13 @@
+import {
+  Container,
+  AppBar,
+  Typography,
+  Grow,
+  Grid,
+  GridList,
+} from "@mui/material";
+import { styled } from "@mui/material/styles";
+
+export const StyledAppBar = styled(AppBar)`
+  border-radius: 1rem;
+`;
